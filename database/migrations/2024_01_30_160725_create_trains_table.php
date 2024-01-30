0d9ae0cdca4f6f19e25618a1f1f0cce6482d7272
elisabichiappini <elisabichiappini@gmail.com>
2024-01-30 19:06:22 +0100

    feat: create table trains and records

--- database/migrations/2024_01_30_160725_create_trains_table.php
+++ database/migrations/2024_01_30_160725_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 30);
+            $table->string('stazione_partenza', 50);
+            $table->string('stazione_arrivo', 50);
+            $table->dateTime('partenza');
+            $table->dateTime('arrivo');
+            $table->smallInteger('codice_treno')->unsigned();
+            $table->tinyInteger('n_carrozze')->unsigned();
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('soppresso')->default(false);
             $table->timestamps();
         });
     }
