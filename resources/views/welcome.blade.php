93356dbf391b2025ca64f9799cb24cb415bba781
elisabichiappini <elisabichiappini@gmail.com>
2024-01-30 19:06:35 +0100

    feat: title project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel Template</title>
+    <title>Train Station</title>
     <!--inclusione assets con direttiva vite-->
     @vite('resources/js/app.js')
     <!--/inclusione assets con direttiva vite-->
 </head>
 <body>
-    <h1>Template</h1>
+    <h1 class="text-center">Train Station</h1>
 </body>
 </html>
