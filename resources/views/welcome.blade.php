54aeafbcefece2443d0a7e4477109cfb669ae609
elisabichiappini <elisabichiappini@gmail.com>
2024-01-30 22:23:34 +0100

    feat: stamp record trains

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,18 @@
     <!--/inclusione assets con direttiva vite-->
 </head>
 <body>
+<header>
     <h1 class="text-center">Train Station</h1>
+</header>
+<main>
+    <section class="container">
+        <h2>Prossimi treni in partenza</h2>
+        <ul class="p-0">
+            @foreach ($trains as $train )
+            <li class="list-group-item"><strong>{{ $train['azienda']}}</strong> partenza da {{ $train['stazione_partenza']}}, {{ $train['orario_partenza'] }} </li>                
+            @endforeach
+        </ul>
+    </section>
+</main>
 </body>
 </html>
